68ab4d3a5b62c48abba25c600e53e54da3e7707c
ranadome <domesandesu@gmail.com>
2021-11-03 12:25:38 +0900

    初期化

---
+++ src/main/java/com/MyDictionary/MyDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.MyDictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDictionaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myDictionary/MyDictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myDictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
